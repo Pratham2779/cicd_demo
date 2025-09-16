1b400058170a89af22418b892c11fb6eac773bd7
Prathamesh Gurav <prathamesh2005gurav@gmail.com>
2025-09-16 18:40:54 +0530

    static portfolio

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import http from 'http';
+import fs from 'fs';
+import path from 'path';
 
-const port=3000;
+const port = 3000;
 
-const server=http.createServer((req,res)=>{
-res.statusCode=200;
-res.setHeader('Content-Type','text/plain');
-res.end('Welcome to Devops world...');
+const server = http.createServer((req, res) => {
+  // Path to index.html (assuming it's in the same folder as this script)
+  const filePath = path.join(process.cwd(), 'index.html');
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error loading index.html');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+  });
 });
 
-server.listen(port,()=>{
-    console.log("server is running on https://portfolio.prathamesh.site");
-})
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
